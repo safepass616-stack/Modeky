157f2d6ab5be7b89e138fa6ae637fc65975997a5
safepass616-stack <safepass616@gmail.com>
2026-06-18 02:27:29 +0200

    commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // App Router doesn't need pageExtensions config
+};
+
+module.exports = nextConfig;
